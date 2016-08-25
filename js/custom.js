ff3eb947ced177a2b7a1eda527987c4552dba795
Soyeb Ahmed <soyeb@kingtidecreative.com>
2016-08-25 18:45:59 +0530

    add video

--- js/custom.js
+++ js/custom.js
@@ -16,6 +16,19 @@ $(function(){
 	else {$('.htc-text').css({'height':'auto'});}
 
 	$('#available').parallax({imageSrc: './images/sky.jpg'});
+
+				
+				$(".youtube").colorbox({iframe:true, innerWidth:740, innerHeight:420});
+				
+				$('.non-retina').colorbox({rel:'group5', transition:'none'})
+				$('.retina').colorbox({rel:'group5', transition:'none', retinaImage:true, retinaUrl:true});
+				
+				//Example of preserving a JavaScript event for inline calls.
+				$("#click").click(function(){ 
+					$('#click').css({"background-color":"#f00", "color":"#fff", "cursor":"inherit"}).text("Open this window again and this message will still be here.");
+					return false;
+				});
+			
 	
 });
 
